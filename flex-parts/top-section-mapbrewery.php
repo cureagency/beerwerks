ce3e5323b8035cf5d29b575dbaa91cf9320aafc9
Maria Mergal <maria@curedesign.co>
2022-07-08 17:24:59 -0400

    ending while loop

--- flex-parts/top-section-mapbrewery.php
+++ flex-parts/top-section-mapbrewery.php
@@ -7,16 +7,16 @@
 				</div>	
 				<div class="map-brewery-top-map acf-map">
 					<div id="map"></div>	
-					<?php 
-				 		$args = array( 'post_type' => 'brewery', 'posts_per_page'=>-1);
-						$loop = new WP_Query( $args);
-				  	?>
-					<?php while ($loop -> have_posts()) : $loop -> the_post(); 
+					<?php while (have_posts()) : the_post();
 						$mapLocation = get_field('map');	
 					?>
 					<div class="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php echo $mapLocation['lng'] ?>">
-						<?php the_field('map'); ?>
+						<p>test</p>
 					</div>
+					<?php 
+				  endwhile;
+				  wp_reset_postdata();
+				  ?>
 				</div>
 			</div>
 		</div>

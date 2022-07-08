4b6d0c07c71ab3493dd82ff6f9950cbc113a0f7f
Maria Mergal <maria@curedesign.co>
2022-07-08 12:41:12 -0400

    testing function

--- functions.php
+++ functions.php
@@ -198,14 +198,14 @@ if( function_exists('acf_add_options_page') ) {
 	
 }
 
-add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+// add_filter('acf/settings/save_json', 'my_acf_json_save_point');
  
-function my_acf_json_save_point( $path ) {
-    // update path
-    $path = get_template_directory_uri() . '/acf-json';
-    // return
-    return $path;
-}
+// function my_acf_json_save_point( $path ) {
+//     // update path
+//     $path = get_template_directory_uri() . '/acf-json';
+//     // return
+//     return $path;
+// }
 
 // add_filter('acf/settings/load_json', 'my_acf_json_load_point');
  

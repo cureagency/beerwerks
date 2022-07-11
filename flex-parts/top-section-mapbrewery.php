217f57bf17bbd55444687476f12a1ea4db1f8d0e
Maria Mergal <maria@curedesign.co>
2022-07-11 09:29:53 -0400

    editing php tags

--- flex-parts/top-section-mapbrewery.php
+++ flex-parts/top-section-mapbrewery.php
@@ -30,13 +30,13 @@
 				<div class ="container container--narrow page-section">
 				<div class="acf-map">
 					
-				  	<?php while ( have_posts()) :  the_post(); 
-					$mapLocation = get_field('map_location');	?>
+				  	<?php while ( have_posts()) { 
+						the_post(); 
+						$mapLocation = get_field('map_location');	
+						?>
 							<div class="marker" data-lat="<?php echo esc_attr($mapLocation['lat']); ?>" data-lng="<?php echo esc_attr($mapLocation['lng']); ?>">
 							</div>
-					<?php 
-				  endwhile;
-				  wp_reset_postdata();
+					<?php }
 				  ?>			
 				</div>
 				</div>

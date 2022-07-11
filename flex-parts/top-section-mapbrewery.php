39c521920ca73ce323f9be50976ce17501db8064
Maria Mergal <maria@curedesign.co>
2022-07-11 13:44:16 -0400

    editing variable

--- flex-parts/top-section-mapbrewery.php
+++ flex-parts/top-section-mapbrewery.php
@@ -24,7 +24,7 @@
 				  		<div class ="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php echo $mapLocation['lng'] ?>">
 						  <p><?php the_title(); ?></a></p>
 						  <?php echo $mapLocation['address'] ?>
-						  <p> <a href="<?php the_permalink() ?>">Learn More</a> &nbsp; <a href="https://www.google.com/maps?saddr=My+Location&daddr=<?php echo $location['address']; ?>" target="_blank"><?php _e('Get Directions','yourtheme'); ?></a>   </p>
+						  <p> <a href="<?php the_permalink() ?>">Learn More</a> &nbsp; <a href="https://www.google.com/maps?saddr=My+Location&daddr=<?php echo $mapLocation['address']; ?>" target="_blank"><?php _e('Get Directions','yourtheme'); ?></a>   </p>
 						</div>	
 					<?php endif; ?>			  
 					<?php 

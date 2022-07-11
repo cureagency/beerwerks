66bbb9cf53cf26fc94b9eebc8d098dab207bc158
Maria Mergal <maria@curedesign.co>
2022-07-11 12:39:58 -0400

    editing address code

--- flex-parts/top-section-mapbrewery.php
+++ flex-parts/top-section-mapbrewery.php
@@ -37,10 +37,10 @@
 				  <?php	
 				  $mapLocation = get_field('map_location');
 				  if (!empty($mapLocation)): ?>
-						<?php	$mapLocation = get_field('map_location');?> 
-						<h3><?php the_title(): ?></h3>
-						<?php echo $mapLocation['address'] ?>
+						<?php	$mapLocation = get_field('map_location');?>
 				  		<div class ="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php echo $mapLocation['lng'] ?>">
+						  <h3><?php the_title(): ?></h3>
+						<?php echo $mapLocation['address'] ?>
 						</div>	
 					<?php endif; ?>			  
 					<?php 
